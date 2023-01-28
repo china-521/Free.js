a906876fb029519703d59f68d4dc6d3efb07dea2
china-521 <3052236335@qq.com>
2023-01-28 17:20:35 +0800

    优化scroll滚动事件

--- utils/dom/event/scroll.js
+++ utils/dom/event/scroll.js
@@ -1,7 +1,9 @@
 import {
 	throttle
 } from "../../../global/utils/throttle.js";
-import { error } from "../../exception/error.js";
+import {
+	error
+} from "../../exception/error.js";
 import {
 	checkNumber
 } from "../../type/checkNumber.js";
@@ -17,6 +19,7 @@ import {
  * 	 - 滚动事件分为两个，分别为向上滚动和向下滚动触发
  *  参数说明：
  *      @param {String/dom} el  添加滚动事件的元素选择器（默认是window）
+ * 		@param {Boolean} flag 开启函数节流
  *      @param {Number} timer  设置函数节流的时间间隔
  * 		@param {Function} callbackStart  滚动到页面顶端要触发的事件
  *      @param {Function} callbackUp  向上滚动要触发的事件
@@ -24,31 +27,47 @@ import {
  */
 export function scroll({
 	el,
+	flag,
 	timer,
 	callbackStart,
 	callbackUp,
 	callbackDown
 }) {
-	if (!checkNumber(timer, false)) {
+	if (timer && !checkNumber(timer, false)) {
 		error("The input types do not match. The timer must be of type Number");
 	}
 	el = el || 'window';
 	timer = timer || 0;
 	if (checkSelector(el)) {
 		let threshold = 0;
-		event(el, 'scroll', throttle(() => {
-			let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-			if (scrollTop <= 0) {
-				(callbackStart)();
-				threshold = scrollTop;
-			} else if (scrollTop >= threshold) {
-				(callbackDown)();
-				threshold = scrollTop;
-			} else {
-				(callbackUp)();
-				threshold = scrollTop;
-			}
-		}, timer));
-
+		if (flag) {
+			event(el, 'scroll', throttle(() => {
+				let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+				if (scrollTop <= 0) {
+					(callbackStart)();
+					threshold = scrollTop;
+				} else if (scrollTop >= threshold) {
+					(callbackDown)();
+					threshold = scrollTop;
+				} else {
+					(callbackUp)();
+					threshold = scrollTop;
+				}
+			}, timer));
+		} else {
+			event(el, 'scroll', () => {
+				let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+				if (scrollTop <= 0) {
+					(callbackStart)();
+					threshold = scrollTop;
+				} else if (scrollTop >= threshold) {
+					(callbackDown)();
+					threshold = scrollTop;
+				} else {
+					(callbackUp)();
+					threshold = scrollTop;
+				}
+			});
+		}
 	}
 }
